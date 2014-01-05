5da7e3f13481e7fcc1baa6e481fca0e5a62b9968
Sabbir Ahmed <sabbiirr@gmail.com>
2014-01-05 20:35:18 +0600

    Update +

--- assets/linker/js/custom.js
+++ assets/linker/js/custom.js
@@ -1,6 +1,7 @@
 (function (io) {
     var socket = io.connect();
     socket.on('connect', function socketConnected () {
+
         socket.on('message', function messageReceived (message) {
 
             log('New message received :: ', message);
@@ -25,7 +26,7 @@ window.log = function log () {
         console.log.apply(console, arguments);
     }
 }
-
+var products_api = '/api/products/';
 var Module = angular.module('main', ['ngRoute']);
 
 Module.config(function ($routeProvider, $locationProvider) {
@@ -43,38 +44,59 @@ Module.config(function ($routeProvider, $locationProvider) {
     $locationProvider.hashPrefix('!');
 })
     .controller('ProductList', function ($scope, $location) {
-        socket.on('update', function () {
-            update($scope, '/api/products');
-        });
         $scope.stat = true;
-        $scope.reverse = true;
-        $scope.toggleForm = function () {
+        $scope.toggleForm = function (mode, id) {
             $scope.stat = !$scope.stat;
             $scope.messages = "";
+            $scope.mode = mode;
+            if (mode == 'edit') {
+                var u = products_api + id;
+                $scope.id = id;
+                socket.get(u, function (product) {
+                    $scope.$apply(function () {
+                        $scope.product = product;
+                    });
+                });
+
+            } else {
+                $scope.product = {};
+            }
 
         };
+        $scope.action = function (mode, id) {
+            if (mode == 'edit') {
+                edit($scope);
+
+            } else if (mode == 'add') {
+                socket.post(products_api, $scope.product, function (message) {
+                    if (message.errors != undefined) {
+                        var msg = [];
+                        for (var item in message.errors[0].ValidationError) {
+                            msg.push('Invalid ' + item + ' !');
+                        }
+                        $scope.$apply(function () {
+                            $scope.messages = msg;
+                            $scope.ms_class = "error";
+                        });
+                    } else {
+                        socket.emit('change');
+                        update($scope, products_api, ["Success !"]);
+                    }
+
+                });
+            } else {
+                console.log(mode);
+            }
+        };
+        update($scope, products_api);
+        socket.on('update', function () {
+            update($scope, products_api);
+        });
+        $scope.reverse = true;
         $scope.sortBy = function (value) {
 
             $scope.reverse = !$scope.reverse;
             $scope.order = value;
-        };
-        $scope.add = function () {
-            socket.post('/api/products', $scope.product, function (message) {
-                if (message.errors != undefined) {
-                    var msg = [];
-                    for (var item in message.errors[0].ValidationError) {
-                        msg.push('Invalid ' + item + ' !');
-                    }
-                    $scope.$apply(function () {
-                        $scope.messages = msg;
-                        $scope.ms_class = "error";
-                    });
-                } else {
-                    socket.emit('change');
-                    update($scope, '/api/products', ["Success !"]);
-                }
-
-            })
         };
 
     })
@@ -89,5 +111,15 @@ function update ($scope, url, msg) {
             $scope.total = products.length;
             $scope.messages = msg || "";
         });
+    });
+}
+function edit($scope) {
+    var url = products_api + $scope.id;
+    socket.put(url, $scope.product, function (res) {
+        socket.emit('change');
+        socket.get(products_api, function (products) {
+            update($scope, products_api, ["Success !"]);
+        });
+
     });
 }
