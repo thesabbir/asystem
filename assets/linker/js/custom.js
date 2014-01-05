ea34f98c14681b3d0c7e90a9fbb1d78e6bd4867f
Sabbir Ahmed <sabbiirr@gmail.com>
2014-01-06 01:12:12 +0600

    Injected Dependency to Work in min.js code.

--- assets/linker/js/custom.js
+++ assets/linker/js/custom.js
@@ -19,32 +19,77 @@ window.log = function log () {
     if (typeof console !== 'undefined') {
         console.log.apply(console, arguments);
     }
-}
+};
 var products_api = '/api/products/';
 var Module = angular.module('main', ['ngRoute']);
+Module.service('shake', function () {
+    var popup = $('.notification');
+    var notify = this.notify = function ($scope, msg) {
+        typeof msg != 'object' ? msg = [msg]: msg;
+        $scope.messages = msg;
+        popup.show(100);
+        popup.fadeOut(3000);
 
-Module.config(function ($routeProvider, $locationProvider) {
-    $routeProvider
-        .when('/products', {
-            controller: 'ProductList',
-            templateUrl: '/templates/products.html'
-        })
-        .when('/', {
-            controller: 'Home',
-            templateUrl: '/templates/home.html'
-        })
+    };
+    this.get = function ($scope, url) {
+        socket.get(url, function (products) {
+            $scope.$apply(function () {
+                $scope.products = products;
+                $scope.total = products.length;
+            });
+        });
+    };
 
-    //$locationProvider.html5Mode('on');
-    $locationProvider.hashPrefix('!');
-})
-    .controller('ProductList', function ($scope, $location) {
+    this.put = function ($scope) {
+        var url = products_api + $scope.id;
+        socket.put(url, $scope.product, function (res) {
+            handle($scope, res);
+
+        });
+    };
+    this.post = function ($scope) {
+        socket.post(products_api, $scope.product, function (res) {
+            handle($scope, res);
+        });
+    };
+    var handle = function ($scope, res) {
+        if (res.errors != undefined) {
+            var msg = [];
+            for (var item in res.errors[0].ValidationError) {
+                msg.push('Invalid ' + item + ' !');
+            }
+            $scope.$apply(function () {
+                notify($scope, msg);
+                $scope.ms_class = "error";
+            });
+        }
+    }
+});
+
+Module.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+        $routeProvider
+            .when('/products', {
+                controller: 'ProductList',
+                templateUrl: '/templates/products.html'
+            })
+            .when('/', {
+                controller: 'Home',
+                templateUrl: '/templates/home.html'
+            });
+
+        //$locationProvider.html5Mode('on');
+        $locationProvider.hashPrefix('!');
+    }])
+
+    .controller('ProductList', ['$scope', '$location', 'shake', function ($scope, $location, shake) {
         //Subscribing to Socket
-        update($scope, products_api);
+        shake.get($scope, products_api);
         //Listening for notification
         socket.on('message', function (message) {
             var msg = message.model.toUpperCase().slice(0, -1) + ' : ' + message.data.name + ' ' + message.verb + 'd ';
 
-            update($scope, products_api, [msg]);
+            shake.get($scope, products_api);
+            shake.notify($scope, msg);
         });
 
         $scope.stat = true;
@@ -68,12 +113,12 @@ Module.config(function ($routeProvider, $locationProvider) {
             }
 
         };
-        $scope.action = function (mode, id) {
+        $scope.action = function (mode) {
             if (mode == 'edit') {
-                edit($scope);
+                shake.put($scope);
 
             } else if (mode == 'add') {
-                add($scope);
+                shake.post($scope);
             } else {
                 console.log(mode);
             }
@@ -85,47 +130,8 @@ Module.config(function ($routeProvider, $locationProvider) {
             $scope.order = value;
         };
 
-    })
-    .controller('Home', function ($scope) {
+    }])
+    .controller('Home', ['$scope', function ($scope) {
 
-    });
+    }]);
 
-function update ($scope, url, msg) {
-    socket.get(url, function (products) {
-        $scope.$apply(function () {
-            $scope.products = products;
-            $scope.total = products.length;
-            if(msg != undefined) {
-                $scope.messages = msg || "";
-                $('.notification').show(100);
-                $('.notification').fadeOut(3000);
-            }
-        });
-    });
-}
-function edit ($scope) {
-    var url = products_api + $scope.id;
-    socket.put(url, $scope.product, function (res) {
-        handle(res);
-
-    });
-}
-function add ($scope) {
-    socket.post(products_api, $scope.product, function (res) {
-        handle(res);
-    });
-};
-function handle (res) {
-    if (res.errors != undefined) {
-        var msg = [];
-        for (var item in res.errors[0].ValidationError) {
-            msg.push('Invalid ' + item + ' !');
-        }
-        $scope.$apply(function () {
-            $scope.messages = msg;
-            $scope.ms_class = "error";
-        });
-    } else {
-        return false;
-    }
-}
