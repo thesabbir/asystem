dd2b7a520142d7488d98020dc9b0803a159d6fe1
Sabbir Ahmed <sabbiirr@gmail.com>
2014-01-07 05:30:17 +0600

    Refactor

--- assets/linker/js/custom.js
+++ assets/linker/js/custom.js
@@ -14,124 +14,127 @@
     window.socket = socket;
 
 })(window.io);
-
+String.prototype.capF = function () {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+}
 window.log = function log() {
     if(typeof console !== 'undefined') {
         console.log.apply(console, arguments);
     }
 };
 
-var Module = angular.module('main', ['ngRoute']);
-Module.factory('Fetch', [function () {
-    return {
-        get: function (url, cb) {
-            socket.get(url, function (all) {
-                return cb(all);
-            });
-        },
-        post: function (url, data, cb) {
-            socket.post(url, data, function (response) {
-                return cb(response);
-            });
-        },
-        update: function (url, data, cb) {
-            socket.put(url, data, function (response) {
-                return cb(response);
-            });
-        }
-    }
-}]);
+var Module = angular.module('main', ['ngRoute', 'ui.bootstrap']);
+Module.service('api', [function () {
+    var prefix = '/api';
+    this.products = prefix + '/products/';
 
+}])
 Module.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
         $routeProvider
             .when('/products', {
-                controller: 'ProductList',
-                templateUrl: '/templates/products.html'
+                controller : 'ProductList',
+                templateUrl : '/templates/products.html',
+                resolve : {
+                    products : function ($q) {
+                        var deferred = $q.defer();
+                        socket.get('/api/products', function (data) {
+                            deferred.resolve(data);
+                        })
+                        return deferred.promise;
+                    }
+                }
             })
             .when('/', {
-                controller: 'Home',
-                templateUrl: '/templates/home.html'
+                controller : 'Home',
+                templateUrl : '/templates/home.html'
             });
 
         //$locationProvider.html5Mode('on');
         $locationProvider.hashPrefix('!');
     }])
 
-    .controller('ProductList', ['$scope', '$location', 'Fetch', function ($scope, $location, Fetch) {
-        var api = '/api/products/';
+    .controller('ProductList', ['$scope', '$location', '$modal', 'products',
+        function ($scope, $location, $modal, products) {
 
-        //Get Initial Data and Subscribe to Products
-        Fetch.get(api, function (products) {
             $scope.products = products;
-            $scope.$apply();
-        });
+            socket.on('message', function (message) {
+                var msg = message.model.slice(0, -1).capF() + ' : ' + message.data.name + ' ' + message.verb + 'd ';
+                $scope.notify({
+                    msg : msg,
+                    type : 'success'
+                })
+                socket.get('/api/products', function (data) {
+                    $scope.$apply(function () {
+                        $scope.products = data;
+                    })
+                })
+            });
 
-        $scope.show = function (action, id) {
-            if(action) {
-                switch (action) {
-                    case 'add':
-                        $scope.product = {}
-                        $scope.mode = action;
-                        break;
-                        $scope.toggleForm('editForm');
-                    case 'edit':
-                        Fetch.get(api+id, function (product) {
-                            $scope.mode = action;
-                            $scope.product = product;
-                            $scope.$apply();
-                        });
-                        $scope.toggleForm('editForm')
-                        break;
-                    case 'details':
-                        Fetch.get(api+id, function (product) {
-                            $scope.product = product;
-                            $scope.$apply();
-                            console.log(product);
-                        });
-
-                        break;
-                    case 'close':
-                        $scope.toggleForm('editForm');
-                        break;
-                    default :
-                        console.log(action);
-                }
+            $scope.openDialog = function (product, mode) {
+                var modalInstance = $modal.open({
+                    templateUrl : '/templates/form.html',
+                    controller : 'FormCtrl',
+                    resolve : {
+                        product : function () {
+                            return product;
+                        },
+                        mode : function () {
+                            return mode;
+                        }
+                    }
+                })
             }
 
-        };
-        $scope.action = function () {
-            if(!$scope.product.id && $scope.mode == 'add') {
-                Fetch.post(api,$scope.product,function (response) {
-                });
-            } else if($scope.product.id && $scope.mode == 'edit') {
-                Fetch.update(api,$scope.product,function (response) {
-                    $scope.notify('Ok');
-                });
-            }
-
-
-        }
-
-    }])
+        }])
     .controller('Boss', ['$scope', function ($scope) {
         $scope.sortBy = function (value) {
 
             $scope.reverse = !$scope.reverse;
             $scope.order = value;
         };
-        $scope.toggleForm = function (form) {
-            if(form) $scope[form] = !$scope[form];
-        };
-        $scope.notify = function (messages) {
-          //  angular.isArray(messages) ? messages = [messages] : console.log('ok');;
-            $scope.notification = false;
+
+        $scope.notices = [];
+        $scope.notify = function (msg) {
+            $scope.notices.push(msg);
+            $scope.$apply();
             setTimeout(function () {
-                $scope.notification = true;
-            }, 200);
+                $scope.closeNotice(0);
+                $scope.$apply();
+            }, 8000);
+        }
+        $scope.closeNotice = function (index) {
+            $scope.notices.splice(index, 1);
         }
 
     }])
     .controller('Home', ['$scope', function ($scope) {
 
-    }]);
+    }])
+    .controller('FormCtrl', function ($scope, api, $modalInstance, product, mode) {
+        var url = api.products + product.id;
+
+        $scope.product = product;
+
+        $scope.submit = function () {
+            switch (mode) {
+                case 'edit':
+                    socket.put(url, product, function (res) {
+                        console.log(res);
+                    });
+                    break;
+                default :
+                    socket.post(api.products, product, function (res) {
+                        console.log(res);
+                    });
+
+            }
+
+            $modalInstance.close('close');
+        };
+
+        $scope.cancel = function () {
+            $modalInstance.dismiss('cancel');
+        };
+
+    })
 
