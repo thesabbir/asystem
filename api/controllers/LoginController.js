c5b6bc460e43324062f0beaf7a5ae29217fa9fa4
Sabbir Ahmed <sabbiirr@gmail.com>
2014-01-08 23:08:46 +0600

    Login Controller Sessions

--- api/controllers/LoginController.js
+++ api/controllers/LoginController.js
@@ -2,17 +2,20 @@ module.exports = {
 
 	create: function (req, res, next) {
 		if (!req.body.username || !req.body.password) {
-			return next({err: 'Username and Password is Required'})
+			return res.json({err: 'Username and Password is Required'})
 		}
-		User.findOneByUsername(req.body.username, function (err, user) {
+		Employees.findOneByUsername(req.body.username, function (err, user) {
 			if (err) return next(err);
-			if (!user) return next({err: 'No user found !'});
+			if (!user) return res.json({err: 'No user found !'});
 
-			bcrypt.compare(req.body.password, user.password, function (err, valid) {
+			require('bcrypt').compare(req.body.password, user.password, function (err, valid) {
 
 				if (err) return next(err);
-				if (!valid) return next({err: 'Password mismatch !'});
-				res.json(user);
+				if (!valid) return res.json({err: 'Password mismatch !'});
+				req.session.authenticated = true;
+				req.session.User = user;
+				res.json({authorized : true, user: user})
+
 
 			});
 
