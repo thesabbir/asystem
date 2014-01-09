098bd9a2f3bf87ebba73aea6952a26701e29e86c
Sabbir Ahmed <sabbiirr@gmail.com>
2014-01-09 22:34:20 +0600

    Started Sales Module

---
+++ assets/linker/js/SalesModule.js
@@ -0,0 +1,45 @@
+angular.module('SalesModule', [])
+   .controller('SalesCtrl', ['$scope', '$rootScope', '$modal', '$api', 'customers',
+      function ($scope, $rootScope, $modal, $api, customers) {
+         $scope.customers = customers;
+         $rootScope.title = $scope.customers.length + " customers & counting";
+         $api.listen($api.customers, function (data, msg) {
+            $scope.customers = data;
+            $scope.notify(msg);
+            $scope.$apply();
+         });
+         $scope.editDialog = function (data, mode) {
+            var modalInstance = $modal.open({
+               templateUrl: '/templates/partials/new_customer.html',
+               controller: 'FormCtrl',
+               resolve: {
+                  fact: function () {
+                     return {
+                        data: data,
+                        mode: mode,
+                        model: $api.customers,
+                        attr: 'customer'
+                     }
+                  }
+               }
+            });
+         };
+         $scope.showDetails = function (customer) {
+            var modalInstance = $modal.open({
+               templateUrl: '/templates/partials/customers_details.html',
+               controller: 'showDetailsCtrl',
+               resolve: {
+                  details: function () {
+                     return {
+                        data: customer,
+                        editDialog: $scope.editDialog,
+                        attr: 'customer'
+                     }
+                  }
+               }
+            });
+         };
+         $scope.deleteIt = function (customer) {
+            $scope.deleteDialog(customer, $api.customers);
+         };
+      }]);
